842b3e66aabf51f6d7eac40ba785b67d4e91d3df
Tri <triphan921@gmail.com>
2021-02-23 11:46:07 -0800

    commit tree dff2f48d4dd3458ce60d2e0d2c7847f0f8d22bde

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+// const superbowlWin = (arr) => arr.find(obj => obj.result === 'W').year
 
+function superbowlWin(record) {
+    const answer = record.find(obj => obj.result === 'W');
+    if (answer !== undefined) {
+        return answer['year']
+    } else {
+        return undefined;
+    }
+}
